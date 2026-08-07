dae90287dbe4424eda205efc22e54c6abdf0a169
Nehal Alam <nehala854@gmail.com>
2026-08-07 23:56:27 +0530

    Remove user insertion and add user retrieval

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from main import User, engine
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Insert 2 users
 u1 = User(name="FromPython", age=20)
 u2 = User(name="Ayza", age=19)
 
@@ -14,7 +13,7 @@ session.commit()
 print("Inserted:", u1.id, u1.name, u1.age)
 print("Inserted:", u2.id, u2.name, u2.age)
 
-# Read all users
+
 users = session.query(User).all()
 print("\nAll users:")
 for u in users:
